6ba3266b98806174f2b9ba31d144f6c505d693a2
Nikolai Vazquez <nikvzqz@gmail.com>
2020-06-21 23:20:14 -0400

    Rename internal bytes methods to contain "_raw"
    Makes it more obvious that it's the internal representation and not the
    bytes contained inside the set.

--- src/lib.rs
+++ src/lib.rs
@@ -705,7 +705,7 @@ impl ByteSet {
     #[inline]
     pub fn rand<R: rand_core::RngCore>(mut rng: R) -> Self {
         let mut set = Self::new();
-        rng.fill_bytes(set.as_bytes_mut());
+        rng.fill_bytes(set.as_raw_bytes_mut());
         set
     }
 
@@ -722,7 +722,7 @@ impl ByteSet {
         mut rng: R,
     ) -> Result<Self, rand_core::Error> {
         let mut set = Self::new();
-        rng.try_fill_bytes(set.as_bytes_mut())?;
+        rng.try_fill_bytes(set.as_raw_bytes_mut())?;
         Ok(set)
     }
 
@@ -1568,25 +1568,25 @@ impl ByteSet {
 
     /// Returns the underlying bytes of `self`.
     #[inline]
-    pub fn into_bytes(self) -> [u8; Self::SIZE] {
+    pub fn into_raw_bytes(self) -> [u8; Self::SIZE] {
         unsafe { mem::transmute(self) }
     }
 
     /// Returns a shared reference to the underlying bytes of `self`.
     #[inline]
-    pub fn as_bytes(&self) -> &[u8; Self::SIZE] {
+    pub fn as_raw_bytes(&self) -> &[u8; Self::SIZE] {
         unsafe { &*self.0.as_ptr().cast() }
     }
 
     /// Returns a mutable reference to the underlying bytes of `self`.
     #[inline]
-    pub fn as_bytes_mut(&mut self) -> &mut [u8; Self::SIZE] {
+    pub fn as_raw_bytes_mut(&mut self) -> &mut [u8; Self::SIZE] {
         unsafe { &mut *self.0.as_mut_ptr().cast() }
     }
 
     /// Returns a shared reference to the underlying bytes of `slice`.
     #[inline]
-    pub fn slice_as_bytes(slice: &[Self]) -> &[u8] {
+    pub fn slice_as_raw_bytes(slice: &[Self]) -> &[u8] {
         let ptr = slice.as_ptr().cast::<u8>();
         let len = slice.len() * Self::SIZE;
         unsafe { slice::from_raw_parts(ptr, len) }
@@ -1594,7 +1594,7 @@ impl ByteSet {
 
     /// Returns a mutable reference to the underlying bytes of `slice`.
     #[inline]
-    pub fn slice_as_bytes_mut(slice: &mut [Self]) -> &mut [u8] {
+    pub fn slice_as_raw_bytes_mut(slice: &mut [Self]) -> &mut [u8] {
         let ptr = slice.as_mut_ptr().cast::<u8>();
         let len = slice.len() * Self::SIZE;
         unsafe { slice::from_raw_parts_mut(ptr, len) }
@@ -1739,12 +1739,12 @@ impl Ord for ByteSet {
 impl hash::Hash for ByteSet {
     #[inline]
     fn hash<H: hash::Hasher>(&self, state: &mut H) {
-        self.as_bytes().hash(state)
+        self.as_raw_bytes().hash(state)
     }
 
     #[inline]
     fn hash_slice<H: hash::Hasher>(data: &[Self], state: &mut H) {
-        Self::slice_as_bytes(data).hash(state)
+        Self::slice_as_raw_bytes(data).hash(state)
     }
 }
 
